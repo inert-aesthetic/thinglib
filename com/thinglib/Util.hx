62e0cc57b809ce4443dad9ea9373630865944011
inert-aesthetic <153579161+inert-aesthetic@users.noreply.github.com>
2025-02-23 22:28:19 +0900

    Move prefab converter to here as it should be MIT license

--- com/thinglib/Util.hx
+++ com/thinglib/Util.hx
@@ -4,6 +4,13 @@ import debug.Logger;
 import thinglib.storage.Reference;
 import uuid.Uuid;
 import thinglib.property.PropertyDef.PropertyType;
+import thinglib.storage.Storage;
+import thinglib.property.Override;
+import thinglib.component.Entity;
+import thinglib.property.core.CoreComponents;
+import pasta.Vect;
+using thinglib.component.util.EntityTools;
+using Lambda;
 #if !js
 import sys.FileSystem;
 #end
@@ -44,6 +51,115 @@ class Util{
         used_name_tails.push(ret);
         return ret;
     }
+
+    public static function convertEntityToPrefabAndReplaceWithInstance(entity:Entity, root:ThingScape, storage:Storage, entityRemovedCallback:Entity->Void):PrefabConversionResult{
+        //1. Save the entity as a file
+        var overrides:Array<Override> = [];
+        if(entity.hasPosition()){
+            var e = entity.asPosition();
+            overrides.push(new Override(Reference.THIS, CoreComponentPosition.X, FLOAT(e.local_position.x)));
+            overrides.push(new Override(Reference.THIS, CoreComponentPosition.Y, FLOAT(e.local_position.y)));
+            entity.asPosition().local_position=new Vect(0,0);
+        }
+        if(entity.isEdge()){
+            var e = entity.asEdge();
+            overrides.push(new Override(Reference.THIS, CoreComponentEdge.A, REF(e.a.guid)));
+            overrides.push(new Override(Reference.THIS, CoreComponentEdge.B, REF(e.b.guid)));
+        }
+        var entity_children = entity.getChildrenRecursive();
+        for(e in entity.getChildrenRecursive(true)){
+            for(c in e.components){
+                for(d in c.definitions){
+                    var val = e.getValue(d);
+                    var is_relevant:Bool = switch(val){
+                        default: false;
+                        case REF(v):
+                            v!=entity.guid&&!entity_children.exists(c->c.guid==v);
+                        case REFS(v):
+                            var res = false;
+                            for(ov in v){
+                                if(ov!=entity.guid&&!entity_children.exists(c->c.guid==ov)){
+                                    res = true;
+                                    break;
+                                }
+                            }
+                            res;
+                    };
+                    if(is_relevant){
+                        var new_override = new Override(e.guid, d.guid, val);
+                        overrides.push(new_override);
+                        switch val {
+                            default:
+                            case REF(v):
+                                e.setValue(d, REF(Reference.EMPTY_ID));
+                            case REFS(v):
+                                e.setValue(d, REFS(v.map(ov->(ov==entity.guid||entity_children.exists(c->c.guid==ov))?ov:Reference.EMPTY_ID)));
+                        }
+                    }
+                }
+            }
+        }
+        var res = storage.save(entity.filename, entity);
+        if(!res){
+            return {success:false};
+        }
+        //2. Re-parent it to the root node
+        if(entityRemovedCallback!=null){
+            entityRemovedCallback(entity);
+        }
+        var parent = entity.parent;
+        //3. Create an instance of the entity at the old entity's location (need 'addAt' type option)
+        var instance = Entity.CreateInstance(root, entity, overrides);
+        var instance_children = instance.getChildrenRecursive();
+        parent.replaceChild(entity, instance);
+        entity.reference.adopt(root);
+        //4. Recurse through all Components in the scene parented to the current construct
+        for(e in instance_children){
+            for(c in e.components){
+                for(d in c.definitions){
+                    var val = e.getValue(d);
+                    switch(val){
+                        default:
+                        case REF(v):
+                            if(entity_children.exists(c->c.guid==v)){
+                                var new_ref = instance_children.find(c->c.guid.unInstancedID==v);
+                                e.setValue(d, REF(new_ref.guid));
+                            }
+                            else if(v==entity.guid){
+                                e.setValue(d, REF(instance.guid));
+                            }
+                        case REFS(v):
+                            var new_refs=[];
+                            var changed=false;
+                            for(ov in v){
+                                if(entity_children.exists(c->c.guid==ov)){
+                                    var new_ref = instance_children.find(c->c.guid.unInstancedID==ov);
+                                    new_refs.push(new_ref.guid);
+                                    changed=true;
+                                }
+                                else if(ov==entity.guid){
+                                    new_refs.push(instance.guid);
+                                    changed=true;
+                                }
+                                else{
+                                    new_refs.push(ov);
+                                }
+                            }
+                            if(changed){
+                                e.setValue(d, REFS(new_refs));
+                            }
+                    }
+                }
+            }
+        }
+
+        return {success:true, instance: instance};
+    }
+}
+
+typedef PrefabConversionResult={
+    success:Bool,
+    ?instance:Entity
 }
 
 abstract ThingID(String) from String to String{
